fc2eb908db9ecfcd8150a68cbf36e9d5553c3ce3
Stanisław Dębicki <71787137+Zwx-tech@users.noreply.github.com>
2025-11-07 12:55:49 +0100

    add: basic php site

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Strona w PHP - Stanisław Dębicki</h1>
+    <h2>Specjalizacja 07.11.2025</h2>
+
+    <p>STRONA 2 - inny slot</p>
+</body>
+</html>
